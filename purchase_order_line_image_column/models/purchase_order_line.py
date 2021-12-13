9dd205d9e312773188aa239dfe8b7ad877ccce62
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-12-13 12:48:35 +0800

    Edit logic

--- purchase_order_line_image_column/models/purchase_order_line.py
+++ purchase_order_line_image_column/models/purchase_order_line.py
@@ -7,14 +7,15 @@ from odoo import api, fields, models
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    request_line_image_ids = fields.Many2many(
-        "purchase.request.line",
-        compute="_compute_request_line_image_ids",
+    images = fields.Many2many(
+        string="Image",
+        attachment=True,
+        compute="_compute_image",
         help="This field refs purchase request line's image. This field not editable.",
     )
 
     @api.depends("product_id")
-    def _compute_request_line_image_ids(self):
+    def _compute_image(self):
         for line in self:
             images = (req_line.image_ids for req_line in line.purchase_request_lines if req_line.image_ids)
             for image in images:
