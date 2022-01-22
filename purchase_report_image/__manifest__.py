b68ff47a7f819e2d73b1443ecaed6efdcf449206
Yoshi Tashiro <tashiro@quartile.co>
2022-01-22 22:31:59 +0800

    [2085][ADD] purchase_report_image

---
+++ purchase_report_image/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+{
+    "name": "Purchase Report Image",
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "website": "https://www.quartile.co",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["purchase_request_line_image"],
+    "data": [
+        "report/purchase_order_templates.xml",
+    ],
+}
